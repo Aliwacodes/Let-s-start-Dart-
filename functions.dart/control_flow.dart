a94f9632e0e24b4d9ca2398eeffe6b912920811e
Aliwacodes <gabbyaliwa@gmail.com>
2024-03-07 14:16:08 +0300

    added control flow

---
+++ functions.dart/control_flow.dart
@@ -0,0 +1,10 @@
+void main() {
+  var number = 8;
+  if (number > 10) {
+    print("Your number is greater than 10");
+  } else if (number < 10) {
+    print("Your number is lesser than 10");
+  } else {
+    print("Your number is equal to 10");
+  }
+}
